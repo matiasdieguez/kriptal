531b09664500ee3a17b753ca31be0cb4b7f78916
Fernando Dieguez <fernandodieguez@hotmail.com.ar>
2018-11-15 16:03:07 -0300

    Cambio en el fondo y agregado custom render para los entrys

---
+++ src/Kriptal/Kriptal.Android/Resources/CustomRenderers/MyEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Android.Content;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+
+namespace Kriptal.Droid.Resources.CustomRenderers
+{
+
+    class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            GradientDrawable gd = new GradientDrawable();
+            gd.SetColor(global::Android.Graphics.Color.Transparent);
+            Control.SetBackgroundDrawable(gd);
+            Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+            Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.White));
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
